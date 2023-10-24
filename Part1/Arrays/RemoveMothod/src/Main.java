be0bc59e16bcaee38a294f444301d1b3b761d731
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-24 17:00:35 +0600

    remove method

--- Part1/Arrays/RemoveMothod/src/Main.java
+++ Part1/Arrays/RemoveMothod/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-//        RemoveMethod
+        RemoveMethod array = new RemoveMethod(10);
+        array.insert(10);
+        array.insert(20);
+        array.insert(30);
+        array.insert(40);
+        array.removeAt(8);
+        array.print();
     }
 }
--- Part1/Arrays/RemoveMothod/src/RemoveMethod.java
+++ Part1/Arrays/RemoveMothod/src/RemoveMethod.java
@@ -1,2 +1,34 @@
 public class RemoveMethod {
+    private int[] items;
+    private int count;
+
+    public RemoveMethod(int length) {
+        items = new int[length];
+    }
+
+    public void insert(int item) {
+        if (items.length == count) {
+            int[] newItems = new int[count * 2];
+//            System.arraycopy(items, 0, newItems, 0, count);
+            System.arraycopy(items, 0, newItems, 0, count);
+
+            items = newItems;
+
+        }
+        items[count++] = item;
+    }
+
+    public void removeAt(int index) {
+        if (index < 0 || index >= count)
+            throw new IllegalArgumentException();
+        for (int i = index; i < count; i++)
+            items[i] = items[i + 1];
+        count--;
+    }
+
+
+    public void print() {
+        for (int i = 0; i < count; i++)
+            System.out.println(items[i]);
+    }
 }
