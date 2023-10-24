cfadda91fc2e789c3da8b2762b2699388a4bbbbc
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-24 10:59:31 +0600

    Insert Method

--- Part1/Arrays/InsertMethod/src/InsertMethod.java
+++ Part1/Arrays/InsertMethod/src/InsertMethod.java
@@ -1,2 +1,23 @@
 public class InsertMethod {
+    private int[] items;
+    private int count;
+
+    public InsertMethod(int length) {
+        items = new int[length];
+    }
+
+    public void insert(int item) {
+        if (items.length == count) {
+            int[] newItems = new int[count * 2];
+            System.arraycopy(items, 0, newItems, 0, count);
+            items = newItems;
+        }
+        items[count++] = item;
+    }
+
+
+    public void print() {
+        for (int i = 0; i < count; i++)
+            System.out.println(items[i]);
+    }
 }
--- Part1/Arrays/InsertMethod/src/Main.java
+++ Part1/Arrays/InsertMethod/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var insertMethod = new InsertMethod(5);
+        insertMethod.insert(10);
+        insertMethod.insert(20);
+        insertMethod.insert(30);
+        insertMethod.insert(40);
+
+        insertMethod.print();
     }
 }
