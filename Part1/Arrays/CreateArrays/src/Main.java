d4fc6aa0fe1f8b6f7fa40a1cde24c7c0cd217bf5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-24 09:55:19 +0600

    array

--- Part1/Arrays/CreateArrays/src/Main.java
+++ Part1/Arrays/CreateArrays/src/Main.java
@@ -1,9 +1,21 @@
-import java.util.Arrays;
+import java.sql.Array;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers.length);
-        System.out.println(Arrays.toString(numbers));
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers.length);
+//        System.out.println(Arrays.toString(numbers));
+
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+
+        numbers.removeAt(1);
+        numbers.print();
+        System.out.println(numbers.indexOf(30));
+
+
+
     }
 }
