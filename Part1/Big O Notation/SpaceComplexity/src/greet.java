6a77c288e0b8394092dfd7ab73fd3af3e9d21723
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-27 23:14:13 +0600

    Space Complexity.

--- Part1/Big O Notation/SpaceComplexity/src/greet.java
+++ Part1/Big O Notation/SpaceComplexity/src/greet.java
@@ -1,34 +1,29 @@
 public class greet {
     public static void show() {
         // Space Complexity.
-
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello World!");
         }
 
         // O(1) space complexity:
-
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello World!");
             System.out.println("Hello World!");
         }
 
         // O(n) space complexity.
-
         int[] arr = new int[10];
         for (int i = 0; i < arr.length; i++) {
             System.out.println("Hello World!");
         }
 
         // O(n^2) space complexity:
-
         int[][] arr2 = new int[10][10];
         for (int i = 0; i < arr2.length; i++) {
             System.out.println("Hello World!");
         }
 
         // O(n^3) space complexity:
-
         int[][][] arr3 = new int[10][10][10];
         for (int i = 0; i < arr3.length; i++) {
             System.out.println("Hello World!");
