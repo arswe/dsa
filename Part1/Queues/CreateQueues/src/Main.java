b7708a335f6214b996b623aed73c3cb0fad0ee77
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-28 21:43:29 +0600

    Queues

--- Part1/Queues/CreateQueues/src/Main.java
+++ Part1/Queues/CreateQueues/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+
+        System.out.println(queue);
     }
 }
