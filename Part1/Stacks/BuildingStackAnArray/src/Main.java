3076fdd7f733037267f6bab504cb082c666878c9
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-28 19:05:04 +0600

    update

---
+++ Part1/Stacks/BuildingStackAnArray/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Stack
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println(stack.peek());
+
+        // push
+        // pop
+        // peek
+        // isEmpty
+        // int[]
+    }
+}
---
+++ Part1/Stacks/BuildingStackAnArray/src/Stack.java
@@ -0,0 +1,36 @@
+import java.nio.channels.IllegalSelectorException;
+import java.util.Arrays;
+
+public class Stack {
+    private final int[] items = new int[5];
+    private int count;
+
+    public void push(int item) {
+        if (count == items.length)
+            throw new StackOverflowError();
+        items[count++] = item;
+    }
+
+    public int pop() {
+        if (count == 0)
+            throw new IllegalSelectorException();
+        return items[--count];
+    }
+
+    public int peek() {
+        if (count == 0)
+            throw new IllegalSelectorException();
+        return items[count - 1];
+    }
+
+    public boolean isEmpty() {
+        return count == 0;
+    }
+
+
+    @Override
+    public String toString() {
+        var content = Arrays.copyOfRange(items, 0, count);
+        return Arrays.toString(content);
+    }
+}
