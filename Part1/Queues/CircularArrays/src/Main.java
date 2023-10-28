1e2e58d6ba47932419644bc74a9bcff8dd52769b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-28 23:08:43 +0600

    Queues build

---
+++ Part1/Queues/CircularArrays/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        var queue = new ArrayQueue(5);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+
+        queue.dequeue();
+        var front = queue.dequeue();
+
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.enqueue(60);
+        queue.enqueue(70);
+        System.out.println(front);
+        System.out.println(queue);
+
+    }
+}
