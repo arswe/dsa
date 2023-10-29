c4ab5e81f0590be8af5bd472e96b7535da138d5e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-29 09:53:22 +0600

    Priority Queues building

--- Part1/Queues/BuildPriorityQueue/src/Main.java
+++ Part1/Queues/BuildPriorityQueue/src/Main.java
@@ -5,5 +5,21 @@ public class Main {
         // [1,3,4,5,2]
         // 0 1 2 3 4
         // items[i + 1] = items[i]
+
+        PriorityQueue queue = new PriorityQueue();
+        queue.add(5);
+        queue.add(2);
+        queue.add(4);
+        queue.add(1);
+        queue.add(3);
+        System.out.println(queue);
+
+        var front = queue.remove();
+        System.out.println("Front: " + front);
+
+        while (!queue.isEmpty())
+            System.out.println(queue.remove());
+
+        System.out.println(queue);
     }
 }
