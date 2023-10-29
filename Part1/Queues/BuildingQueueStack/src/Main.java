1ac4d775804a9822c009bc42238361293586e3b0
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-29 09:29:50 +0600

    Building a Queue Using a Stack

--- Part1/Queues/BuildingQueueStack/src/Main.java
+++ Part1/Queues/BuildingQueueStack/src/Main.java
@@ -3,5 +3,17 @@ public class Main {
         // Q [10,20,30]
         // S1 [10,20,30]
         // S2 [30,20,10]
+
+        QueueWithTwoStack queue = new QueueWithTwoStack();
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
+        var front = queue.dequeue();
+        System.out.println(front);
     }
 }
