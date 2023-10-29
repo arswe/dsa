2a8957ec142509187845fc0e8caa9092045f16b6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-29 09:42:11 +0600

    Priority Queues building

---
+++ Part1/Queues/BuildPriorityQueue/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Priority Queue
+        // Insert(5)
+        // [1,3,4,5,2]
+        // 0 1 2 3 4
+        // items[i + 1] = items[i]
+    }
+}
