e072160589476a02a30e04385951707a5d24701c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-29 09:34:44 +0600

    Priority Queues

--- Part1/Queues/PriorityQueues/src/Main.java
+++ Part1/Queues/PriorityQueues/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Priority Queues world!");
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(5);
+        pq.add(1);
+        pq.add(3);
+        pq.add(4);
+        pq.add(2);
+
+        while (!pq.isEmpty())
+            System.out.println(pq.remove());
     }
 }
