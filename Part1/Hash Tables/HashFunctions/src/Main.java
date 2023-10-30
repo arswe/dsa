a931df781c03f1b509b40169457bcb2262b1a62b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-30 09:11:15 +0600

    Hash Functions

--- Part1/Hash Tables/HashFunctions/src/Main.java
+++ Part1/Hash Tables/HashFunctions/src/Main.java
@@ -1,8 +1,13 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> map = Map.of(1, "one", 2, "two", 3, "three");
-        System.out.println(map);
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "A");
+        map.put(2, "B");
+        map.put(3, "C");
+
+        System.out.println(map.get(1));
     }
 }
