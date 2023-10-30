9b2f8ed6811eed7da0265d1c7af803132ce43ce1
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-30 09:02:46 +0600

    Hash Functions

---
+++ Part1/Hash Tables/HashFunctions/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> map = Map.of(1, "one", 2, "two", 3, "three");
+        System.out.println(map);
+    }
+}
