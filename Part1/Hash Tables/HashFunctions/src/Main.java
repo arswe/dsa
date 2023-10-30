a6f5711b34a0374e749acc8dfa7552acfbaa1bd8
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-30 09:31:24 +0600

    hashing

--- Part1/Hash Tables/HashFunctions/src/Main.java
+++ Part1/Hash Tables/HashFunctions/src/Main.java
@@ -3,18 +3,21 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        String str = "orange";
+
+        System.out.println(str.hashCode());
+
         Map<String, String> map = new HashMap<>();
         map.put("123456-A", "a");
-
         System.out.println(hash("123456 - A"));
     }
 
     public static int hash(String Key) {
         int hash = 0;
-        for (var ch: Key.toCharArray()) {
+
+        for (var ch : Key.toCharArray()) {
             hash += ch;
         }
         return hash % 100;
     }
-
 }
