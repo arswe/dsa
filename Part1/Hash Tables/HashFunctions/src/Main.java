63f0039b60c2653725b3ae747c87b2c077ac81d5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-10-30 09:28:47 +0600

    update

--- Part1/Hash Tables/HashFunctions/src/Main.java
+++ Part1/Hash Tables/HashFunctions/src/Main.java
@@ -3,11 +3,18 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "A");
-        map.put(2, "B");
-        map.put(3, "C");
+        Map<String, String> map = new HashMap<>();
+        map.put("123456-A", "a");
 
-        System.out.println(map.get(1));
+        System.out.println(hash("123456 - A"));
     }
+
+    public static int hash(String Key) {
+        int hash = 0;
+        for (var ch: Key.toCharArray()) {
+            hash += ch;
+        }
+        return hash % 100;
+    }
+
 }
