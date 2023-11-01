05daab6de7e86e038d46c6ba751ed7b1e4426a95
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-01 17:00:48 +0600

    Level Order Traversal

---
+++ Part2/Binary Trees/Level Order Traversal/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        var tree = new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+
+        tree.traversePreOrder();
+
+    }
+}
