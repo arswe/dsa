1580c726f5dbb25b900ef7176e10986237c0e67a
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-01 12:10:37 +0600

    TraversingTrees

---
+++ Part2/Binary Trees/TraversingTrees/src/TraversingTrees.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TraversingTrees {
+    // Traversing Trees Formulas:
+    // 1. Preorder Traversal - Root, Left, Right
+    // 2. Inorder Traversal - Left, Root, Right
+    // 3. Postorder Traversal - Left, Right, Root
+}
