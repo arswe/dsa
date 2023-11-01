416f0e75d3950566e07f2bceca3e46b858974f51
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-01 13:26:28 +0600

    Depth First Traversals

--- Part2/Binary Trees/DepthFirstTraversals/src/Main.java
+++ Part2/Binary Trees/DepthFirstTraversals/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Depth First Traversals:");
+        var tree = new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+
+        tree.traversePreOrder();
+        System.out.println();
+
+        tree.traverseInOrder();
+        System.out.println();
+
+        tree.traversePostOrder();
+        System.out.println();
     }
 }
---
+++ Part2/Binary Trees/DepthFirstTraversals/src/Tree.java
@@ -0,0 +1,98 @@
+public class Tree {
+    private class Node {
+        private final int value;
+        private Node leftChild;
+        private Node rightChild;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node=" + value;
+        }
+    }
+
+    private Node root;
+
+    // Insert Method:
+    public void insert(int value) {
+        var node = new Node(value);
+
+        if (root == null) {
+            root = node;
+            return;
+        }
+
+        var current = root;
+        while (true) {
+            if (value < current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = node;
+                    break;
+                }
+
+                current = current.leftChild;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = node;
+                    break;
+                }
+
+                current = current.rightChild;
+            }
+        }
+    }
+
+    // Find Method:
+    public boolean find(int value) {
+        var current = root;
+        while (current != null) {
+            if (value < current.value) current = current.leftChild;
+            else if (value > current.value) current = current.rightChild;
+            else return true;
+        }
+
+        return false;
+    }
+
+    // Traverse Pre-Order Method: (Overloaded)
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    // Traverse Pre-Order Method: (Recursive)
+    private void traversePreOrder(Node root) {
+        if (root == null) return;
+        System.out.println(root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
+
+    // Traverse In-Order Method: (Overloaded)
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    // Traverse In-Order Method: (Recursive)
+    private void traverseInOrder(Node root) {
+        if (root == null) return;
+        traverseInOrder(root.leftChild);
+        System.out.println(root.value);
+        traverseInOrder(root.rightChild);
+    }
+
+    // Traverse Post-Order Method: (Overloaded)
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    // Traverse Post-Order Method: (Recursive)
+    private void traversePostOrder(Node root) {
+        if (root == null) return;
+        traversePostOrder(root.leftChild);
+        traversePostOrder(root.rightChild);
+        System.out.println(root.value);
+    }
+}
