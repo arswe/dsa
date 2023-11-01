5ce6abc778877c7483aaab209f947310f4b0ec07
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-01 16:31:34 +0600

    Depth and Height of Nodes Minimum Value in a Tree Equality Checking

---
+++ Part2/Binary Trees/Equality Checking/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        var tree = new Tree();
+        tree.insert(7);
+        tree.insert(4);
+        tree.insert(9);
+        tree.insert(1);
+        tree.insert(6);
+        tree.insert(8);
+        tree.insert(10);
+
+        var tree2 = new Tree();
+        tree2.insert(7);
+        tree2.insert(4);
+        tree2.insert(9);
+        tree2.insert(1);
+        tree2.insert(6);
+        tree2.insert(8);
+//        tree2.insert(10);
+
+        System.out.println(tree.equals(null));
+    }
+}
