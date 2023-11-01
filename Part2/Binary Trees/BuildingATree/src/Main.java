972d6b39c742478cb9015f993b443fc62fd93d82
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-01 11:15:41 +0600

    Building a Tree

---
+++ Part2/Binary Trees/BuildingATree/src/Main.java
@@ -0,0 +1,14 @@
+import com.sun.source.tree.Tree;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Building a Tree");
+
+        // Tree class (root) - root is the top node in a tree
+        // Node class (value, left, right) - left and right are children of the node
+        // insert(value) - insert a new node with the given value
+        // find(value): boolean
+        // lookup(value) - return the node with the given value
+        // remove(value) - remove the node with the given value
+    }
+}
