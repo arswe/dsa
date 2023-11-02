03a59cf232c8cf8933d2249fa7f092fb58c9777b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-02 22:46:48 +0600

    Building Heap Remoeve method again practic

--- Part2/Heaps/HeapRemove/src/Heap.java
+++ Part2/Heaps/HeapRemove/src/Heap.java
@@ -11,7 +11,56 @@ public class Heap {
         bubbleUp();
     }
 
-    //
+    // Removal in a heap is O(log n)
+    public int remove() {
+
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        items[0] = items[--size];
+
+        var index = 0;
+        while (index <= size && !isValidParent(index)) {
+            var largerChildIndex = largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index = largerChildIndex;
+        }
+        return 0;
+    }
+
+    private boolean isEmpty() {
+        return size == 0;
+    }
+
+    private int largerChildIndex(int index) {
+        return (leftChild(index) > rightChild(index)) ?
+                leftChildIndex(index) : rightChildIndex(index);
+    }
+
+    private boolean isValidParent(int index) {
+        return items[index] >= leftChild(index) &&
+                items[index] >= rightChild(index);
+    }
+
+    // Left Child item: 2 * index + 1
+    private int leftChild(int index) {
+        return items[leftChildIndex(index)];
+    }
+
+    // Right Child item: 2 * index + 2
+    private int rightChild(int index) {
+        return items[rightChildIndex(index)];
+    }
+
+    // left child: 2 * index + 1
+    private int leftChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+    // right child: 2 * index + 2
+    private int rightChildIndex(int index) {
+        return index * 2 + 2;
+    }
 
 
     private boolean isFull() {
