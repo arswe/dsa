fd341ce820e00d503f5ac19e63b37379e69e9a17
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-02 10:56:50 +0600

    Detecting Rotations

---
+++ Part2/AVL Trees/DetectingRotations/src/AVLTree.java
@@ -0,0 +1,71 @@
+public class AVLTree {
+    private class AVLNode {
+        private int height;
+        private int value;
+        private AVLNode leftChild;
+        private AVLNode rightChild;
+
+        public AVLNode(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Value=" + this.value;
+        }
+    }
+
+    private AVLNode root;
+
+    // insert a value into the tree
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    private AVLNode insert(AVLNode root, int value) {
+        if (root == null) {
+            return new AVLNode(value);
+        }
+        if (value < root.value) {
+            root.leftChild = insert(root.leftChild, value);
+        } else {
+            root.rightChild = insert(root.rightChild, value);
+        }
+
+        root.height = Math.max(
+                height(root.leftChild),
+                height(root.rightChild)) + 1;
+
+        balance(root);
+        return root;
+    }
+
+    private void balance(AVLNode root) {
+        if (isLeftHeavy(root)) {
+            if (balanceFactor(root.leftChild) < 0)
+                System.out.println("Left rotate " + root.leftChild.value);
+            System.out.println("Right rotate " + root.value);
+        } else if (isRightHeavy(root)) {
+            if (balanceFactor(root.rightChild) > 0)
+                System.out.println("Right rotate " + root.rightChild.value);
+            System.out.println("Left rotate " + root.value);
+        }
+    }
+
+    private boolean isLeftHeavy(AVLNode node) {
+        return balanceFactor(node) > 1;
+    }
+
+    private boolean isRightHeavy(AVLNode node) {
+        return balanceFactor(node) < -1;
+    }
+
+    private int balanceFactor(AVLNode node) {
+        return (node == null) ? 0 : height(node.leftChild) - height(node.rightChild);
+    }
+
+    private int height(AVLNode node) {
+        return (node == null) ? -1 : node.height;
+    }
+
+}
