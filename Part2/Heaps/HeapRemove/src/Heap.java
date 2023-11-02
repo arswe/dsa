b17654736a04d4c399a237748f6650e4e7520582
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-02 22:34:41 +0600

    Building Heap Remoeve method again practic

--- Part2/Heaps/HeapRemove/src/Heap.java
+++ Part2/Heaps/HeapRemove/src/Heap.java
@@ -11,51 +11,8 @@ public class Heap {
         bubbleUp();
     }
 
-    // Removal in a heap is O(log n)
-    public int remove() {
-        if (isEmpty())
-            throw new IllegalStateException();
+    //
 
-        items[0] = items[--size];
-
-        var index = 0;
-        while (index <= size && !isValidParent(index)) {
-            var largerChildIndex = largerChildIndex(index);
-            swap(index, largerChildIndex);
-            index = largerChildIndex;
-        }
-        return index;
-    }
-
-    private boolean isEmpty() {
-        return size == 0;
-    }
-
-    private int largerChildIndex(int index) {
-        return Math.max((leftChildIndex(index)), (rightChildIndex(index)));
-    }
-
-    private boolean isValidParent(int index) {
-        return items[index] >= leftChild(index) &&
-                items[index] >= rightChild(index);
-    }
-
-
-    private int leftChild(int index) {
-        return items[leftChildIndex(index)];
-    }
-
-    private int rightChild(int index) {
-        return items[rightChildIndex(index)];
-    }
-
-    private int leftChildIndex(int index) {
-        return index * 2 + 1;
-    }
-
-    private int rightChildIndex(int index) {
-        return index * 2 + 2;
-    }
 
     private boolean isFull() {
         return size == items.length;
