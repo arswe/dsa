d5ac9646eb7296a1b5c6e388a871dfabd5558dbc
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-03 11:49:58 +0600

    Remoeve

---
+++ Part2/Tries/RemoveWord/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("car");
+        trie.insert("care");
+
+        trie.remove("car");
+
+
+    }
+}
