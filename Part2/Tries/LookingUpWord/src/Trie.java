a593b73fbe45c17b6d53b48b59a320b5a7735386
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-03 11:20:32 +0600

    Looking Up a Word

---
+++ Part2/Tries/LookingUpWord/src/Trie.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+
+public class Trie {
+    public static int ALPHABET_SIZE = 26;
+
+    private class Node {
+        private int value;
+        private HashMap<Character, Node> children = new HashMap<>();
+        private boolean isEndOfWord;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Value: " + value;
+        }
+
+        public boolean hasChild(char ch) {
+            return children.containsKey(ch);
+        }
+
+        public void addChild(char ch) {
+            children.put(ch, new Node(ch));
+        }
+
+        public Node getChild(char ch) {
+            return children.get(ch);
+        }
+
+    }
+
+    private Node root = new Node(' ');
+
+    public void insert(String word) {
+        Node current = root;
+        for (char ch : word.toCharArray()) {
+            if (!current.hasChild(ch))
+                current.addChild(ch);
+            current = current.getChild(ch);
+        }
+        current.isEndOfWord = true;
+    }
+}
