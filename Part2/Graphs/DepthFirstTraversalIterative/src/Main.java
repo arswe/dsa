8ed080f83a9b97e25c725cb42ddb29baa5b4bb4e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-04 10:12:39 +0600

    Depth-first Traversal (Iterative)

---
+++ Part2/Graphs/DepthFirstTraversalIterative/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        var graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addNode("D");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "D");
+        graph.addEdge("D", "C");
+        graph.addEdge("A", "C");
+
+        graph.depthFirstTraversalIter("C");
+    }
+}
