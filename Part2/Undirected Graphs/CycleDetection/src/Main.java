db1be82c7b86264383f588d731d3ebfb11ec0b44
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-05 08:48:05 +0600

    Cycle Detection (Undirected Graphs)

---
+++ Part2/Undirected Graphs/CycleDetection/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        var graph = new WeightedGraph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("A", "B", 0);
+        graph.addEdge("B", "C", 0);
+        graph.addEdge("C", "A", 0);
+
+        System.out.println(graph.hasCycle());
+    }
+}
