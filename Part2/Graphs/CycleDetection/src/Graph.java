3f27aa210f5b98f6b733eafb12f759fd2190474f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-05 07:22:13 +0600

    Cycle Detection (Directed Graphs)

---
+++ Part2/Graphs/CycleDetection/src/Graph.java
@@ -0,0 +1,172 @@
+import java.util.*;
+
+public class Graph {
+    private class Node {
+        private String label;
+
+        public Node(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+    }
+
+    private Map<String, Node> nodes = new HashMap<>();
+    private Map<Node, List<Node>> adjacencyList = new HashMap<>();
+
+    public void addNode(String label) {
+        var node = new Node(label);
+        nodes.putIfAbsent(label, node);
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdge(String from, String to) {
+        var fromNode = nodes.get(from);
+        if (fromNode == null) throw new IllegalArgumentException();
+
+        var toNode = nodes.get(to);
+        if (toNode == null) throw new IllegalArgumentException();
+
+        adjacencyList.get(fromNode).add(toNode); // Directed graph only
+//        adjacencyList.get(toNode).add(fromNode); // Undirected graph only
+    }
+
+    public void print() {
+        for (var source : adjacencyList.keySet()) {
+            var targets = adjacencyList.get(source);
+            if (!targets.isEmpty()) System.out.println(source.label + " is connected to " + targets);
+        }
+    }
+
+    public void removeNode(String label) {
+        var node = nodes.get((label));
+        if (node == null) return;
+
+        for (var n : adjacencyList.keySet())
+            adjacencyList.get(n).remove(node);
+
+        adjacencyList.remove(node);
+        nodes.remove(node);
+    }
+
+    public void removeEdge(String from, String to) {
+        var fromNode = nodes.get(from);
+        var toNode = nodes.get(to);
+
+        if (fromNode == null || toNode == null) return;
+
+        adjacencyList.get(fromNode).remove(toNode);
+//        adjacencyList.get(toNode).remove(fromNode);
+    }
+
+    public void depthFirstTraversalRec(String root) {
+        var node = nodes.get(root);
+        if (node == null) return;
+
+        depthFirstTraversalRec(node, new HashSet<>());
+    }
+
+    private void depthFirstTraversalRec(Node root, Set<Node> visited) {
+        System.out.println(root);
+        visited.add(root);
+
+        for (var node : adjacencyList.get(root))
+            if (!visited.contains(node)) depthFirstTraversalRec(node, visited);
+    }
+
+    public void depthFirstTraversalIter(String root) {
+        var node = nodes.get(root);
+        if (node == null) return;
+
+        Set<Node> visited = new HashSet<>();
+        Stack<Node> stack = new Stack<>();
+        stack.push(node);
+
+        while (!stack.isEmpty()) {
+            var current = stack.pop();
+
+            if (visited.contains(current)) continue;
+
+            System.out.println(current);
+            visited.add(current);
+
+            for (var neighbor : adjacencyList.get(current))
+                if (!visited.contains(neighbor)) stack.push(neighbor);
+        }
+    }
+
+    public void breadthFirstTraversal(String root) {
+        var node = nodes.get(root);
+        if (node == null) return;
+
+        Set<Node> visited = new HashSet<>();
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            var current = queue.remove();
+
+            if (visited.contains(current)) continue;
+
+            System.out.println(current);
+            visited.add(current);
+
+            for (var neighbor : adjacencyList.get(current))
+                if (!visited.contains(neighbor)) queue.add(neighbor);
+        }
+    }
+
+    public List<String> topologicalSort() {
+        Stack<Node> stack = new Stack<>();
+        Set<Node> visited = new HashSet<>();
+
+        for (var node : nodes.values())
+            topologicalSort(node, visited, stack);
+        List<String> sorted = new ArrayList<>();
+
+        while (!stack.isEmpty())
+            sorted.add(stack.pop().label);
+        return sorted;
+    }
+
+    private void topologicalSort(Node node, Set<Node> visited, Stack<Node> stack) {
+        if (visited.contains(node)) return;
+
+        visited.add(node);
+        for (var neighbor : adjacencyList.get(node))
+            topologicalSort(neighbor, visited, stack);
+        stack.push(node);
+    }
+
+    public boolean hasCycle() {
+        Set<Node> all = new HashSet<>();
+        all.addAll(nodes.values());
+        Set<Node> visiting = new HashSet<>();
+        Set<Node> visited = new HashSet<>();
+
+        while (!all.isEmpty()) {
+            var current = all.iterator().next();
+            if (hasCycle(current, all, visiting, visited)) return true;
+        }
+        return false;
+    }
+
+    private boolean hasCycle(Node node, Set<Node> all, Set<Node> visiting, Set<Node> visited) {
+        all.remove(node);
+        visiting.add(node);
+
+        for (var neighbor : adjacencyList.get(node)) {
+            if (visited.contains(neighbor)) continue;
+            if (visiting.contains(neighbor)) return true;
+            if (hasCycle(neighbor, all, visiting, visited)) return true;
+        }
+
+        visiting.remove(node);
+        visited.add(node);
+
+        return false;
+    }
+}
---
+++ Part2/Graphs/CycleDetection/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        var graph = new Graph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addNode("D");
+
+        graph.addEdge("A", "B");
+        graph.addEdge("B", "C");
+        graph.addEdge("C", "A");
+
+        System.out.println(graph.hasCycle());
+
+
+    }
+}
