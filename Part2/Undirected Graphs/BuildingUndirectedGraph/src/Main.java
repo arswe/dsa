36a30edf2c8af8d47ab50a8f787a8ba1869e68e9
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-05 07:32:07 +0600

    Undirected Graphs  Weighted Graphs

--- Part2/Undirected Graphs/BuildingUndirectedGraph/src/Main.java
+++ Part2/Undirected Graphs/BuildingUndirectedGraph/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Undirected Graphs!");
+
+        var graph = new WeightedGraph();
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+
+        graph.addEdge("A", "B", 3);
+        graph.addEdge("B", "C", 2);
+        graph.addEdge("A", "C", 1);
+
+        graph.print();
+
     }
 }
