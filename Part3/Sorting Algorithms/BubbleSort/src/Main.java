84e8b66de1cc8ef8b4ef9c7ee7d7a70af594d76e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-05 09:30:23 +0600

    Bubble Sort Implementation

--- Part3/Sorting Algorithms/BubbleSort/src/Main.java
+++ Part3/Sorting Algorithms/BubbleSort/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {5, 1, 4, 2, 8, 0, 2};
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sort(array);
+
+        System.out.println("Sorted array:" + Arrays.toString(array));
     }
 }
