9433d4610719891e8ad420579237f784406dc78c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-06 23:13:17 +0600

    binarySearch Iterative start

---
+++ Part3/Searching Algorithms/BinarySearchIterative/src/Search.java
@@ -0,0 +1,16 @@
+public class Search {
+    public int binarySearch(int[] arr, int target) {
+        return binarySearch(arr, target, 0, arr.length - 1);
+    }
+
+    private int binarySearch(int[] arr, int target, int left, int right) {
+        if (left > right)
+            return -1;
+
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == target)
+            return binarySearch(arr, target, left, mid - 1);
+        return binarySearch(arr, target, mid + 1, right);
+    }
+}
