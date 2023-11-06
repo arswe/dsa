0cf703a6191503119f4c30d04136a1c5cb606152
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-06 23:09:56 +0600

    update

---
+++ Part3/Searching Algorithms/BinarySearch/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var binarySearch = new Search();
+        var arr = new int[]{1, 2, 3, 4, 5, 6, 7};
+        var result = binarySearch.binarySearch(arr, 1);
+        System.out.println(result);
+    }
+}
---
+++ Part3/Searching Algorithms/BinarySearch/src/Search.java
@@ -0,0 +1,32 @@
+public class Search {
+    public int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (target == arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int binarySearch(int[] arr, int target) {
+        return binarySearch(arr, target, 0, arr.length - 1);
+    }
+
+    private int binarySearch(int[] arr, int target, int left, int right) {
+        if (left > right) {
+            return -1;
+        }
+
+        var middle = (left + right) / 2;
+
+        if (target == arr[middle]) {
+            return middle;
+        }
+
+        if (target < arr[middle]) {
+            return binarySearch(arr, target, left, middle - 1);
+        }
+
+        return binarySearch(arr, target, middle + 1, right);
+    }
+}
