2db3a70154a340ea8fec5b67f26f2964c086c38e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-06 23:23:07 +0600

    Ternary Search

--- Part3/Searching Algorithms/TernarySearch/src/Main.java
+++ Part3/Searching Algorithms/TernarySearch/src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5, 6, 7, 8, 9};
+        int[] arr = {1, 2, 3, 5, 6, 8, 9, 10, 12, 14, 15};
         Search search = new Search();
-        int target = 5;
-        int result = search.TernarySearch(arr, target, 0, arr.length - 1);
-        if (result == -1) {
-            System.out.println("Element not found");
-        } else {
-            System.out.println("Element found at index " + result);
-        }
+        System.out.println(search.TernarySearch(arr, 6));
     }
 }
--- Part3/Searching Algorithms/TernarySearch/src/Search.java
+++ Part3/Searching Algorithms/TernarySearch/src/Search.java
@@ -1,22 +1,27 @@
 public class Search {
-    public int TernarySearch(int[] arr, int target, int left, int right) {
-        if (right >= left) {
-            int mid1 = left + (right - left) / 3;
-            int mid2 = right - (right - left) / 3;
-            if (arr[mid1] == target) {
-                return mid1;
-            }
-            if (arr[mid2] == target) {
-                return mid2;
-            }
-            if (target < arr[mid1]) {
-                return TernarySearch(arr, target, left, mid1 - 1);
-            } else if (target > arr[mid2]) {
-                return TernarySearch(arr, target, mid2 + 1, right);
-            } else {
-                return TernarySearch(arr, target, mid1 + 1, mid2 - 1);
-            }
+    public int TernarySearch(int[] array, int target) {
+        return TernarySearch(array, target, 0, array.length - 1);
+    }
+
+    private int TernarySearch(int[] arr, int target, int left, int right) {
+        if (left > right) {
+            return -1;
         }
-        return -1;
+        int partitionSize = (right - left) / 3;
+        int mid1 = left + partitionSize;
+        int mid2 = right - partitionSize;
+        if (arr[mid1] == target) {
+            return mid1;
+        }
+        if (arr[mid2] == target) {
+            return mid2;
+        }
+        if (target < arr[mid1]) {
+            return TernarySearch(arr, target, left, mid1 - 1);
+        }
+        if (target > arr[mid2]) {
+            return TernarySearch(arr, target, mid2 + 1, right);
+        }
+        return TernarySearch(arr, target, mid1 + 1, mid2 - 1);
     }
 }
