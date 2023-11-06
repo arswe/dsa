80eede82a25c8fe8a280504d91248a89be89e5ed
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-06 23:16:23 +0600

    binarySearch Iterative done

--- Part3/Searching Algorithms/BinarySearchIterative/src/Main.java
+++ Part3/Searching Algorithms/BinarySearchIterative/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Binary Search- Iterative!");
+        var search = new Search();
+        var numbers = new int[]{1, 2, 3, 4, 5};
+        var index = search.binarySearch(numbers, 2);
+        System.out.println(index);
     }
 }
--- Part3/Searching Algorithms/BinarySearchIterative/src/Search.java
+++ Part3/Searching Algorithms/BinarySearchIterative/src/Search.java
@@ -1,16 +1,19 @@
 public class Search {
     public int binarySearch(int[] arr, int target) {
-        return binarySearch(arr, target, 0, arr.length - 1);
-    }
+        var left = 0;
+        var right = arr.length - 1;
 
-    private int binarySearch(int[] arr, int target, int left, int right) {
-        if (left > right)
-            return -1;
+        while (left <= right) {
+            var mid = left + (right - left) / 2;
 
-        int mid = (left + right) / 2;
+            if (arr[mid] == target)
+                return mid;
 
-        if (arr[mid] == target)
-            return binarySearch(arr, target, left, mid - 1);
-        return binarySearch(arr, target, mid + 1, right);
+            if (arr[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return -1;
     }
 }
