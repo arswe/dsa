8db7e6cedff91812767de7f6eb53b86cef8b5d7f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-06 23:01:48 +0600

    update

--- Part3/Searching Algorithms/LinearSearch/src/Main.java
+++ Part3/Searching Algorithms/LinearSearch/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 7, 8, 9, 10};
         Search search = new Search();
-        int[] arr = {1, 2, 3, 4, 5};
-        int target = 3;
-        int result = search.linearSearch(arr, target);
+
+        var result = search.linearSearch(numbers, 7);
         System.out.println(result);
     }
 }
--- Part3/Searching Algorithms/LinearSearch/src/Search.java
+++ Part3/Searching Algorithms/LinearSearch/src/Search.java
@@ -1,11 +1,8 @@
 public class Search {
     public int linearSearch(int[] arr, int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (target == arr[i]) {
+        for (int i = 0; i < arr.length; i++)
+            if (target == arr[i])
                 return i;
-            }
-        }
         return -1;
     }
-
 }
