c4d62bd0168491477f59cd840174f3a818335aba
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-07 09:25:10 +0600

    Count Vowels

--- Part3/String Manipulation Algorithms/CountVowels/src/Main.java
+++ Part3/String Manipulation Algorithms/CountVowels/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var count = StringUtils.countVowels("Hello World");
+        var count = StringUtils.countVowels("HellO World");
         System.out.println(count);
     }
 }
--- Part3/String Manipulation Algorithms/CountVowels/src/StringUtils.java
+++ Part3/String Manipulation Algorithms/CountVowels/src/StringUtils.java
@@ -1,8 +1,11 @@
 public class StringUtils {
     public static int countVowels(String str) {
+        if (str == null)
+            return 0;
+
         int count = 0;
         String vowels = "aeiou";
-        for (var ch : str.toCharArray()) {
+        for (var ch : str.toLowerCase().toCharArray()) {
             if (vowels.indexOf(ch) != -1)
                 count++;
         }
