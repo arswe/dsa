2d16833aa2b455c137f551bbf65f352f6aaf17af
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-10 15:53:21 +0600

    String Remove Duplicates

--- Part3/String Manipulation Algorithms/RemoveDuplicates/src/StringUtils.java
+++ Part3/String Manipulation Algorithms/RemoveDuplicates/src/StringUtils.java
@@ -3,8 +3,13 @@ import java.util.Set;
 
 public class StringUtils {
     public static String RemoveDuplicates(String s) {
+
+        if (s == null)
+            return "";
+
         StringBuilder output = new StringBuilder();
         Set<Character> seen = new HashSet<>();
+
 
         for (var ch : s.toCharArray()) {
             if (!seen.contains(ch)) {
