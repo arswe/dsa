ee5b7661f7429acd15fbe0ff5813f3c88fc0f63d
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-10 16:04:59 +0600

    String sentence Capitalization

--- Part3/String Manipulation Algorithms/SentenceCapitalization/src/Utils.java
+++ Part3/String Manipulation Algorithms/SentenceCapitalization/src/Utils.java
@@ -1,5 +1,9 @@
 public class Utils {
     public static String SentenceCapitalization(String sentence) {
+        if (sentence == null || sentence.isEmpty()) {
+            return "";
+        }
+
         String[] words = sentence.split(" ");
 
         for (int i = 0; i < words.length; i++) {
