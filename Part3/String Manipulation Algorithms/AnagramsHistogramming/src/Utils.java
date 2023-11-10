f0f0fef8d545db89b499ca46ab210e8c67914e46
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-10 16:16:09 +0600

    Anagrams Use Histogramming

--- Part3/String Manipulation Algorithms/AnagramsHistogramming/src/Utils.java
+++ Part3/String Manipulation Algorithms/AnagramsHistogramming/src/Utils.java
@@ -4,24 +4,29 @@ public class Utils {
         final int ENG_ALPHABET_LENGTH = 26;
         int[] frequencies = new int[ENG_ALPHABET_LENGTH];
 
+        first = first.toLowerCase();
+        second = second.toLowerCase();
 
         for (var i = 0; i < first.length(); i++) {
             char ch = first.charAt(i);
-            if (!Character.isLetter(ch))
-                continue;
-            frequencies[ch - 'a']++;
+            if (Character.isLetter(ch)) {
+                frequencies[ch - 'a']++;
+            }
         }
 
         for (var i = 0; i < second.length(); i++) {
             char ch = second.charAt(i);
-            if (!Character.isLetter(ch))
-                continue;
-            frequencies[ch - 'a']--;
+            if (Character.isLetter(ch)) {
+                frequencies[ch - 'a']--;
+            }
         }
 
-        for (var frequency : frequencies)
-            if (frequency != 0)
+
+        for (var i = 0; i < ENG_ALPHABET_LENGTH; i++) {
+            if (frequencies[i] != 0) {
                 return false;
+            }
+        }
 
         return true;
 
