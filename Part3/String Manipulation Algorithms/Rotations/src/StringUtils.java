4ccd697d2e833955aa2e598fa3835daf8fc3bf5e
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-10 15:47:25 +0600

    String Rotations

--- Part3/String Manipulation Algorithms/Rotations/src/StringUtils.java
+++ Part3/String Manipulation Algorithms/Rotations/src/StringUtils.java
@@ -1,5 +1,7 @@
 public class StringUtils {
     public static boolean areRotations(String str1, String str2) {
-        return (str1.length() == str2.length()) && ((str1 + str1).contains(str2));
+        if (str1.length() != str2.length())
+            return false;
+        return (str1 + str1).contains(str2);
     }
 }
