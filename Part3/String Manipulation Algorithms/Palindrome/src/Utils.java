3c368119f73513eaa53ba0982df674830cf0b50f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-11-10 16:24:21 +0600

    Palindrome

--- Part3/String Manipulation Algorithms/Palindrome/src/Utils.java
+++ Part3/String Manipulation Algorithms/Palindrome/src/Utils.java
@@ -1,7 +1,22 @@
 public class Utils {
     public static boolean isPalindrome(String word) {
-        var input = new StringBuilder(word);
-        var reversed = input.reverse().toString();
-        return word.equals(reversed);
+//        var input = new StringBuilder(word);
+//        var reversed = input.reverse().toString();
+//        return word.equals(reversed);
+
+
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
